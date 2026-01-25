310e8b2eea7cf96eef2247d46082238ea699337c
alokgoldy <alokkrgoldy@gmail.com>
2026-01-25 23:22:43 +0530

    fix: resolve main tsx file

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App"
+
+const queryClient = new QueryClient();
+
+<QueryClientProvider client={queryClient}>
+  <App/>
+</QueryClientProvider>
