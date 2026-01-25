7cec1f6f9fc17be5e4ca6f7c280f7a247c93769c
alokgoldy <alokkrgoldy@gmail.com>
2026-01-25 23:58:42 +0530

    feat: added task toggle api

--- src/api/task.api.ts
+++ src/api/task.api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Task } from "../features/tasks/task.types";
 
 const api = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
@@ -7,4 +8,11 @@ const api = axios.create({
 export const fetchTasks = async () =>{
     const res = await api.get("/todos?_limit=10");
     return res.data;
+}
+
+export const toggleTask = async (task: Task): Promise<Task> => {
+  const res = await api.patch(`/todos/${task.id}`, {
+    completed: !task.completed
+  });
+  return res.data;
 }
