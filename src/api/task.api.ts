ac95cf17614705d03ff8c7ef19349b77087ae303
alokgoldy <alokkrgoldy@gmail.com>
2026-01-28 23:10:52 +0530

    fix: fixed task

--- src/api/task.api.ts
+++ src/api/task.api.ts
@@ -1,18 +1,18 @@
-import axios from "axios";
-import type { Task } from "../features/tasks/task.types";
+import axios from 'axios';
+import type { Task } from '../features/tasks/task.types';
 
 const api = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
+  baseURL: 'https://jsonplaceholder.typicode.com',
 });
 
-export const fetchTasks = async () =>{
-    const res = await api.get("/todos?_limit=10");
-    return res.data;
-}
+export const fetchTasks = async () => {
+  const res = await api.get('/todos?_limit=10');
+  return res.data;
+};
 
 export const toggleTask = async (task: Task): Promise<Task> => {
   const res = await api.patch(`/todos/${task.id}`, {
-    completed: !task.completed
+    completed: !task.completed,
   });
   return res.data;
-}
+};
